ba886498cdab72f0ae668b4b18763f2dc67a535f
thotranuet97@gmail.com <thotranuet97@gmail.com>
2018-06-29 08:34:26 +0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,36 @@
+Rails.application.routes.draw do
+  resources :posts
+  get 'comment/new'
+
+  get 'comment/show'
+
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
+  get 'users/new'
+
+  get 'users/show'
+
+  root 'static_pages#home'
+  get 'home' => 'static_pages#home'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'signup' => 'users#new'
+  resources :users 
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :posts,          only: [:create, :destroy, :show]
+  resources :comments
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships,       only: [:create, :destroy]
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
